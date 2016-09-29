dd9fb915721cbb37b2014c25e9d186ba1fd7f804
billba <billba@microsoft.com>
2016-09-29 09:28:00 +0900

    more functionality

--- bot.js
+++ bot.js
@@ -22,12 +22,37 @@ bot.dialog('/',
     new builder
     .IntentDialog({ recognizers: [new builder.LuisRecognizer(process.env.LUIS_APP_MODEL)] })
     .matches('builtin.intent.communication.send_text', [
-        (session, args) => {
+        (session, args, next) => {
             let message = builder.EntityRecognizer.findEntity(args.entities, 'builtin.communication.message');
-            message = message && message.entity;
             let contact_name = builder.EntityRecognizer.findEntity(args.entities, 'builtin.communication.contact_name');
-            contact_name = contact_name && contact_name.entity;
-            session.send(`You asked to send the message "${message}" to "${contact_name}"`);
+            session.dialogData.info = { message: message && message.entity, contact_name: contact_name && contact_name.entity };
+
+            if (!session.dialogData.info.contact_name)
+                builder.Prompts.text(session, "Who do you want to message");
+            else
+                next();
+        },
+        (session, args, next) => {
+            if (!session.dialogData.info.contact_name)
+                session.dialogData.info.contact_name = args.response;
+
+            if (!session.dialogData.info.message)
+                builder.Prompts.text(session, `What do you want to tell ${session.dialogData.info.contact_name}`);
+            else
+                next();
+        },
+        (session, args, next) => {
+            if (!session.dialogData.info.message)
+                session.dialogData.info.message = args.response;
+
+            builder.Prompts.choice(session, `Do you want to send the message "${session.dialogData.info.message}" to "${session.dialogData.info.contact_name}"?`, ["Yes", "No"]);
+        },
+        (session, args, next) => {
+            if (args.response.entity === "Yes") {
+                session.send("Message sent.");
+            } else {
+                session.send("Message not sent.");
+            }
         }
     ])
     .onDefault(
